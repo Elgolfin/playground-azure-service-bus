d9a3676b67afb3c291b7ee6c651e66e0859c6b03
Elgolfin <elgo@nicomputer.net>
2018-08-22 21:07:51 -0400

    Add the application insights logger

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
 
 namespace playground_azure_service_bus
 {
@@ -6,6 +9,27 @@ namespace playground_azure_service_bus
     {
         static void Main(string[] args)
         {
+            // Adding JSON file into IConfiguration.
+            IConfiguration config =  new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            // Read instrumentation key from IConfiguration.
+            string ikey = config["ApplicationInsights:InstrumentationKey"];
+            
+            TelemetryConfiguration.Active.InstrumentationKey = ikey;
+            TelemetryClient log = new TelemetryClient();
+            log.TrackTrace("Demo application starting up.");
+
+            for (int i = 0; i < 10; i++)
+            {
+                log.TrackEvent("Testing " + i);
+            }
+
+            log.TrackException(new Exception("Demo exception."));
+            log.TrackTrace("Demo application exiting.");
+            log.Flush();
+
             Console.WriteLine("Hello World!");
         }
     }
