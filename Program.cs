d7c0a71ed5a6ce306eaa1e94bfc8a3c12d0ad003
Elgolfin <elgo@nicomputer.net>
2018-08-22 20:19:35 -0400

    Add the .net core base console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace playground_azure_service_bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
